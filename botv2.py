76191e2a57791c13184220221d2345f216e0b8ea
camiprzlarra <camperez2022@udec.cl>
2025-08-20 19:58:47 -0400

    botv2.py

--- botv2.py
+++ botv2.py
@@ -165,7 +165,7 @@ Intrucciones:
 - Analiza la pregunta cuidadosamente
 - Desarrolla pensamiento paso a paso
 - Responde solo preguntas que este relacionadas al topico en el que eres experto
-- La respuesta no puede ser mayor a 2000 caracteres, en caso de ser mayor debes resumirla
+- La respuesta no puede ser mayor a 2000 caracteres
 
 
 Responde de manera clara y breve la siguiente pregunta:
@@ -184,7 +184,7 @@ Instrucciones:
 - Analiza la pregunta cuidadosa mente
 - Desarrolla pensamiento paso a paso
 - Responde solo preguntas que este relacionadas al topico en el que eres experto
-- La respuesta no puede ser mayor a 2000 caracteres, en caso de ser mayor debes resumirla
+- La respuesta no puede ser mayor a 2000 caracteres
 
 Responde de manera clara y de manera breve la siguiente pregunta:
 {question}
@@ -274,6 +274,7 @@ async def on_message(message):
 if __name__ == "__main__":
 
     bot.run(TOKEN_KEY)
+
 
 
 
